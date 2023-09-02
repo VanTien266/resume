588ee835338053bed73887b19c9dcda900470c51
Tien <luuvantien2006@gmail.com>
2023-09-02 15:45:53 +0700

    hanlde responsive

--- src/components/informations/Personal.tsx
+++ src/components/informations/Personal.tsx
@@ -10,9 +10,9 @@ import Image from 'next/image';
 const Personal = (props: any) => {
   const { personalInformations } = props;
   return (
-    <div className="flex gap-2 mb-2 h-96">
-      <div className="bg-[#313c4e] w-3.5 rounded-xl"></div>
-      <div className="flex w-full md:w-2/3 p-5 items-center bg-[#ebebeb] rounded-l-lg">
+    <div className="flex flex-col gap-2 mb-2 h-full lg:flex-row lg:h-96">
+      <div className="bg-[#313c4e] w-full rounded-xl lg:w-3.5"></div>
+      <div className="flex flex-col w-full p-5 items-center bg-[#ebebeb] rounded-l-lg sm:flex-row lg:w-2/3">
         <div className="rounded-full border-2 border-[#449399]">
           <Image
             src="/avatar.jpeg"
@@ -22,9 +22,9 @@ const Personal = (props: any) => {
             className="rounded-full"
           />
         </div>
-        <div className="flex flex-1 flex-col px-4">
-          <div className="text-5xl text-[#313c4e]">Luu Van Tien</div>
-          <div className="text-2xl text-[#449399]">Fullstack Developer</div>
+        <div className="flex flex-1 flex-col gap-1 sm:px-4">
+          <div className="text-center text-5xl text-[#313c4e] sm:text-left">Luu Van Tien</div>
+          <div className="text-center text-2xl text-[#449399] sm:text-left">Fullstack Developer</div>
           <div className="text-base text-justify">
             Frontend Developer, with experience in many fields of Web
             development, always looking for new knowledge and willing to learn.
@@ -35,9 +35,9 @@ const Personal = (props: any) => {
           </div>
         </div>
       </div>
-      <div className="md:w-1/3 flex h-full p-7 flex-col justify-between bg-[#313c4e] rounded-r-lg">
+      <div className="flex flex-col gap-2 h-full w-full p-7 justify-between bg-[#313c4e] rounded-r-lg lg:w-1/3 ">
         {personalInformations.map((item: any) => (
-          <div key={item.key} className="flex">
+          <div key={item.key} className="flex items-center">
             <div className="w-12 h-12 rounded-full border-2 border-[#449399] p-2 mr-2">
               {item.icon}
             </div>

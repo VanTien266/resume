d1fcec9ca3ea02ae3cc0b5d9ae5a44f96dce9232
Tien <luuvantien2006@gmail.com>
2023-11-11 21:10:37 +0700

    minor change

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,24 +1,17 @@
-'use client';
+"use client";
 
-import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
-import { useTheme } from 'next-themes';
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 function Options() {
-	const { theme, setTheme } = useTheme();
-
-	return (
-		<button
-			type='button'
-			className='fixed right-4 top-6 md:right-6 md:top-8 lg:right-10 lg:top-10'
-			onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-		>
-			{theme === 'light' ? (
-				<MoonIcon className='w-8 h-8 fill-[#449399]' />
-			) : (
-				<SunIcon className='w-8 h-8 fill-[#449399]' />
-			)}
-		</button>
-	);
+  return (
+    <button
+      type="button"
+      className="fixed right-4 top-6 md:right-6 md:top-8 lg:right-10 lg:top-10"
+    >
+      <MoonIcon className="w-8 h-8 fill-[#449399]" />
+      <SunIcon className="w-8 h-8 fill-[#449399]" />
+    </button>
+  );
 }
 
 Options.defaultProps = {};
